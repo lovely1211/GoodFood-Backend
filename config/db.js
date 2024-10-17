31c61ec5a090edb24f3c2db1d2a7648209436cc4
Lovely Singh <lovely1211zmn@gmail.com>
2024-10-17 14:07:37 +0530

    db connection

--- config/db.js
+++ config/db.js
@@ -1,10 +1,14 @@
-// config/db.js  YPpQLujLH6Cd9PFj
+// config/db.js
+// df0vvXezxf0vLGp9
 
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://lovely1211zmn: <YPpQLujLH6Cd9PFj>@clusterfood.ai8f5.mongodb.net/?retryWrites=true&w=majority&appName=ClusterFood');
+        await mongoose.connect(
+'mongodb+srv://lovely1211zmn:df0vvXezxf0vLGp9@clustergoodfood.6pm1d.mongodb.net/?retryWrites=true&w=majority&appName=ClusterGoodFood'
+        );
+
         console.log('MongoDB connected');
     } catch (err) {
         console.error('MongoDB connection error:', err);
